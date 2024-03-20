d268fb03e881194260e9f9e165c40ff562e0128f
Daniel-4h <daniel.ahrndsen@unibas.ch>
2024-03-20 16:59:13 +0100

    formulation adjustments

--- Intro/lexicon.py
+++ Intro/lexicon.py
@@ -3,37 +3,33 @@ class Lexicon:
     next = "Weiter"
 
     #Consent Text
-    consent_p1 ='Vielen Dank für Ihr Interesse, an dieser Studie teilzunehmen. <br> Diese Studie wird im Rahmen einer Abschlussarbeit unter der Leitung von Prof. Ulf J.J. Hahnel und Zahra Rahmani, Fakultät für Psychologie, Universität Basel, Schweiz, von Daniel Ahrndsen, Student der Psychologie, durchgeführt.'
-    consent_p2 = 'Wenn Sie an dieser Studie teilnehmen möchten, werden Sie gebeten, Folgendes zu tun:'
+    consent_p1 ='Herzlichen Dank für Ihr Interesse an unserer Studie. <br> Diese Studie wird von Daniel Ahrndsen im Rahmen seiner Abschlussarbeit durchgeführt und steht unter der Leitung von Prof. Ulf J.J. Hahnel und Zahra Rahmani, Fakultät für Psychologie der Universität Basel.'
+    consent_p2 = 'Was Ihre Teilnahme beinhaltet:'
 
-    consent_bullet1 = 'Einen Satz von Online-Fragebögen auszufüllen. Möglicherweise werden Sie gebeten, zu bewerten, wie gut bestimmte Aussagen oder Persönlichkeitsmerkmale auf Sie zutreffen.'
-    consent_bullet2= 'Sie werden auch gebeten, einige demografische Informationen anzugeben, darunter Ihr Alter, Geschlecht, politische Orientierung und Einkommen.'
-    consent_bullet3 = 'Sie werden gebeten, Ihre Einstellung zu verschiedenen politischen Vorschlägen anzugeben.'
+    consent_bullet1 = 'Wir werden Sie nach Ihrer Einstellung zu verschiedenen politischen Vorschlägen fragen.'
+    consent_bullet2 = 'Außerdem werden Sie bitten, eine Reihe von Online-Fragebögen auszufüllen. Dabei sollen Sie bewerten, inwiefern bestimmte Aussagen auf Sie zutreffen.'
+    consent_bullet3= 'Zuletzt bitten wir Sie darum, einige demografische Informationen anzugeben, unter anderem Ihr Alter, Ihr Geschlecht, Ihre politische Orientierung und Ihr Einkommen.'
     
     
     ## Achtung hier richtige dauer
-    duration ='Dauer'
+    duration ='Dauer:'
     consent_p3 ='Die Teilnahme an dieser Studie dauert etwa <b>10 Minuten</b>.'
 
     ## Achtung hier richtige vergütung und währung und marktforschungsinstitut
     risks = 'Risiken und Vorteile:'
-    consent_p4 = 'Es sind keine Risiken im Zusammenhang mit Ihrer Teilnahme an dieser Studie bekannt. Ihre Teilnahme kann dazu beitragen, offene Forschungsfragen in der Verhaltens- und Umweltpsychologie besser zu verstehen. Eine Teilnahme an dieser Studie bleibt freiwillig.'
+    consent_p4 = 'Es sind keine Risiken im Zusammenhang mit Ihrer Teilnahme an dieser Studie verbunden. Durch Ihre Teilnahme unterstützen Sie die Erforschung offener Fragestellungen in der Umweltpsychologie.'
 
     privacy = 'Datenschutz:'
-    consent_p5 = 'Ihre Daten werden zu jeder Zeit streng vertraulich behandelt. Die Daten können niemals direkt mit Ihrer Identität verknüpft werden. Niemand, einschließlich der Forschenden dieser Studie, wird in der Lage sein, Ihre Identität offenzulegen. Die Daten werden in anonymisierter Form auf Open-Science-Plattformen verfügbar gemacht. Alle sensiblen Informationen, die potenziell zur Offenlegung Ihrer Identität verwendet werden könnten, werden aus dem Datensatz entfernt, bevor die Daten veröffentlicht werden. Wir, die Forschenden des Projekts, werden auch niemals die Identität eines der Teilnehmer kennen, es sei denn, Sie offenbaren sie uns, indem Sie uns per E-Mail kontaktieren.'
+    consent_p5 = 'Der Schutz Ihrer Daten hat für uns höchste Priorität. Ihre Angaben werden zu jeder Zeit streng vertraulich behandelt und nicht mit Ihnen persönlichen Informationen verknüpft. Auch die an dieser Studie Beteiligten, haben keinen Zugang zu den identifizierenden Informationen, außer Sie entscheiden sich, uns direkt zu kontaktieren. Nach Abschluss der Arbeit an dieser Studie werden die Daten in anonymisierter Form auf Open-Science-Plattformen veröffentlicht. Davor werden alle sensiblen Informationen aus dem Datensatz entfernt.'
 
-    consent_p6 = 'Die Teilnahme an dieser Studie ist freiwillig. Sie können die Teilnahme jederzeit ohne Nachteile ablehnen oder abbrechen.'
+    consent_p6 = 'Die Teilnahme an dieser Studie ist freiwillig. Sie können die Teilnahme jederzeit ablehnen oder abbrechen.'
 
-    consent_p8 = 'Wenn Ihnen etwas an der Studie oder Ihrer Teilnahme unklar ist, wenn Sie Fragen haben oder ein forschungsbezogenes Problem melden möchten, senden Sie eine E-Mail an daniel.ahrndsen@unibas.ch'
+    consent_p8 = 'Bei Unklarheiten oder Fragen, senden Sie bitte eine E-Mail an daniel.ahrndsen@unibas.ch'
 
-    consent_p9 = 'Indem Sie auf "Weiter" klicken, erklären Sie Ihr Einverständnis zur Teilnahme an dieser Studie und geben an, dass Sie '
-
-    consent_bullet4 = 'die angegebenen Bedingungen gelesen und verstanden haben und genügend Zeit hatten, um eine Entscheidung zu treffen'
-    consent_bullet5 = 'mindestens 18 Jahre alt sind'
-    consent_bullet6 = 'die deutsche Staatsbürgerschaft besitzen.'
+    consent_p9 = 'Mit einem Klick auf „Weiter“ bestätigen Sie, dass Sie die obenstehenden Informationen gelesen und verstanden haben und sich bewusst für eine Teilnahme entscheiden.'
 
     consent_p10 = 'Wenn Sie teilnehmen möchten, kreuzen Sie bitte die untenstehenden Kästchen an und klicken Sie dann auf "Weiter".'
    
-    data_consent1 = 'Ich bin einverstanden mit der Nutzung der Daten für wissenschaftliche Zwecke und/oder die Publikation der Ergebnisse dieser Studie in wissenschaftlichen Zeitschriften oder Büchern jeweils unter der Voraussetzung, dass die Daten anonymisiert werden und keine Rückschlüsse auf meine Identität gezogen werden können.'
-    data_consent2 = 'Ich bin einverstanden mit der Nutzung der Daten für Lehrzwecke (Kurse und Vorträge für Studierende und Berufstätige, die jeweils zu einem vertraulichen Umgang verpflichtet sind).'
+    data_consent1 = 'Ich bin einverstanden mit der Nutzung der anonymisierten Daten für wissenschaftliche Zwecke und/oder für eine Publikation der Ergebnisse dieser Studie in wissenschaftlichen Zeitschriften oder Büchern.'
+    data_consent2 = 'Ich bin einverstanden mit der Nutzung der anonymisierten Daten für Lehrzwecke (Kurse und Vorträge für Studierende und Berufstätige, die jeweils zu einem vertraulichen Umgang verpflichtet sind).'
     
